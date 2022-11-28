c4d96baa4f188f7aacab8e7e3fddac58c5041d93
Slava <slava91petrushin@yandex.ru>
2022-11-28 22:19:36 +0300

    fix set registration-email-resending

--- src/repositories/clients-db-repository.ts
+++ src/repositories/clients-db-repository.ts
@@ -46,7 +46,7 @@ class ClientsRepositoryModel {
 	public async 	updateClient(id: string, code: string, expirationData: Date): Promise<ModifyResult<ApiTypes.IClientDB> | null>{
 		try {
 			console.log(id, code,expirationData);
-			return await clientsCollection.findOneAndUpdate({id}, {$set: [{'emailConfirmation.code': code}, {'emailConfirmation.expirationData': expirationData}]} );
+			return await clientsCollection.findOneAndUpdate({id}, {$set: {'emailConfirmation.code': code, 'emailConfirmation.expirationData': expirationData}} );
 		} catch (error) {
 			console.error(`ClientsRepositoryModel, Not updateClient: `, error);
 			return null;
